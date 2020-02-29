b8e4811a5515c69d9955f8e90f8acdfe1f3935f2
eric <liaogd79@gmail.com>
2020-02-29 21:09:31 +0800

    update

--- message.go
+++ message.go
@@ -1,13 +1,24 @@
 package ws_chat
 
 const (
-	MsgEvent MessageEvent = "msg"
+	MsgEvent      MessageEvent = "msg"
+	DownloadEvent MessageEvent = "download"
 )
 
 type MessageEvent string
 
+const (
+	Success MessageType = "success"
+	Warning MessageType = "warning"
+	Info    MessageType = "info"
+	Error   MessageType = "error"
+)
+
+type MessageType string
+
 type message struct {
 	to    string
 	Event MessageEvent `json:"event"`
+	Type  MessageType  `json:"type"`
 	Data  interface{}  `json:"data"`
 }
